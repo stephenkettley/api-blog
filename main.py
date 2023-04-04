60308d165b159bb766093d11f06822399aaf1c64
Stephen Kettley <kettleystephen@gmail.com>
2023-04-04 10:21:57 +0200

    created 3 more routes

--- main.py
+++ main.py
@@ -4,19 +4,28 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/")
-def index() -> dict:
-    return {
-        "data": {
-            "name": "Stephen",
-            "age": 25,
-        }
-    }
+@app.get("/blog/all")
+def get_all_blogs() -> dict:
+    """Get all blogs."""
+    return {"data": "list of all blogs"}
 
 
-@app.get("/about")
-def about() -> dict:
-    return {"data": "about page"}
+@app.get("/blog/unpublished")
+def get_all_unpublished_blogs() -> dict:
+    """Get all unpublished blogs."""
+    return {"data": "list of all unpublished blogs"}
+
+
+@app.get("/blog/{id}")
+def get_one_blog(id: int) -> dict:
+    """Get a blog with specific id."""
+    return {"data": f"blog with id {id}"}
+
+
+@app.get("/blog/{id}/comments")
+def show_blog_comments(id: int) -> dict:
+    """Show comments of blog with specific id."""
+    return {"data": f"comments of blog with id {id}"}
 
 
 if __name__ == "__main__":
